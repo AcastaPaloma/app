8a7c4c5e99c6bd925fe4cb2a966239551dc1b5b7
AcastaPaloma <kuanyi.wang0807@gmail.com>
2021-12-06 19:34:28 -0500

    Modification 1

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 UPLOADED_FILE_DIR_PATH = os.path.join(os.path.dirname(__file__), "static", "uploaded-images")
 UPLOADED_ICON_FILE_DIR_PATH = os.path.join(os.path.dirname(__file__), "static", "discord_server_icons")
 
+
 def check_to_create_table():
     connection = sqlite3.connect('database2.sqlite')
     cur = connection.cursor()
@@ -17,6 +18,7 @@ def check_to_create_table():
         'CREATE TABLE IF NOT EXISTS Artpieces ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, artist_name TEXT NOT NULL, image_name TEXT NOT NULL, email TEXT NOT NULL, message TEXT NOT NULL, image_filename TEXT NOT NULL, artpiece_type TEXT NOT NULL)')
 
     connection.close()
+
 
 def check_to_create_table_discord_server_info():
     connection = sqlite3.connect('database2.sqlite')
@@ -26,8 +28,10 @@ def check_to_create_table_discord_server_info():
 
     connection.close()
 
+
 check_to_create_table()
 check_to_create_table_discord_server_info()
+
 
 @app.route('/')
 def home():
@@ -53,7 +57,7 @@ def home():
     cur.execute(formatted_query, query_args)
     returned_paintings = cur.fetchall()
 
-   #-----------------------------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------------------
 
     cur.execute("SELECT * FROM Artpieces WHERE artpiece_type='Drawing'")
     records_of_drawing = cur.fetchall()
@@ -88,30 +92,55 @@ def home():
 
     wanted_ids3 = (random.sample(list_of_id3, 4))
     tupled_wanted_ids3 = tuple(wanted_ids3)
-    questionmarks3 = '?' * len(tupled_wanted_ids)
+    questionmarks3 = '?' * len(tupled_wanted_ids3)
 
     formatted_query3 = 'SELECT * FROM Artpieces WHERE id IN({})'.format(', '.join(questionmarks3))
     query_args3 = []
     query_args3.extend(tupled_wanted_ids3)
 
     cur.execute(formatted_query3, query_args3)
-    records_of_digital_art = cur.fetchall()
+    returned_digital_art = cur.fetchall()
 
+    # -----------------------------------------------------------------------------------------------------------
+
+    cur.execute("SELECT * FROM Artpieces WHERE artpiece_type='Sculpture'")
+    records_of_sculpture = cur.fetchall()
+    connection.commit()
+
+    list_of_id4 = []
+
+    for element in records_of_sculpture:
+        list_of_id4.append(element[0])
+
+    wanted_ids4 = (random.sample(list_of_id4, 4))
+    tupled_wanted_ids4 = tuple(wanted_ids4)
+    questionmarks4 = '?' * len(tupled_wanted_ids4)
+
+    formatted_query4 = 'SELECT * FROM Artpieces WHERE id IN({})'.format(', '.join(questionmarks4))
+    query_args4 = []
+    query_args4.extend(tupled_wanted_ids4)
+
+    cur.execute(formatted_query4, query_args4)
+    returned_sculptures = cur.fetchall()
 
     connection.commit()
     connection.close()
 
-
     return render_template('home.html', records_of_painting=records_of_painting
-                            , records_of_drawing=records_of_drawing
-                            , returned_paintings = returned_paintings
-                           , returned_drawings = returned_drawings
-                           ,records_of_digital_art = records_of_digital_art
+                           , records_of_drawing=records_of_drawing
+                           , records_of_digital_art=records_of_digital_art
+                           , returned_paintings=returned_paintings
+                           , records_of_sculpture=records_of_sculpture
+                           , returned_drawings=returned_drawings
+                           , returned_digital_art=returned_digital_art
+                           , returned_sculptures=returned_sculptures
                            )
+
 
 @app.route('/search')
 def search():
     return render_template('search_for_art.html', records='')
+
 
 @app.route('/search_results', methods=["POST"])
 def search_results():
@@ -119,13 +148,16 @@ def search_results():
 
     connection = sqlite3.connect('database2.sqlite')
     cur = connection.cursor()
-    cur.execute("SELECT * FROM Artpieces WHERE name Like ? OR artist_name Like ? OR image_name Like ? OR artpiece_type Like ?", (requested_search,requested_search, requested_search, requested_search))
+    cur.execute(
+        "SELECT * FROM Artpieces WHERE name Like ? OR artist_name Like ? OR image_name Like ? OR artpiece_type Like ?",
+        (requested_search, requested_search, requested_search, requested_search))
     records = cur.fetchall()
 
     connection.commit()
     connection.close()
     return render_template('search_for_art.html',
                            records=records)
+
 
 @app.route("/details_of_art", methods=["POST", "GET"])
 def art_details():
@@ -137,23 +169,20 @@ def art_details():
     requested_image_filename = request.values.get('image_filename')
     requested_artpiece_type = request.values.get('image_filename')
 
-
     return render_template('details_of_art.html',
-                           requested_name = requested_name,
-                           requested_image_name = requested_image_name,
-                           requested_artist_name = requested_artist_name,
-                           requested_email = requested_email,
-                           requested_message = requested_message,
-                           requested_image_filename = requested_image_filename,
-                           requested_artpiece_type = requested_artpiece_type)
-
-
-
+                           requested_name=requested_name,
+                           requested_image_name=requested_image_name,
+                           requested_artist_name=requested_artist_name,
+                           requested_email=requested_email,
+                           requested_message=requested_message,
+                           requested_image_filename=requested_image_filename,
+                           requested_artpiece_type=requested_artpiece_type)
 
 
 @app.route('/about')
 def about():
     return render_template('page2.html')
+
 
 @app.route("/process", methods=["POST"])
 def process():
@@ -164,7 +193,6 @@ def process():
     message = request.values.get('message')
     type = request.form["options"]
 
-
     image_file = request.files['image']
     my_filename = secure_filename(image_file.filename)
 
@@ -173,7 +201,9 @@ def process():
 
     connection = sqlite3.connect('database2.sqlite')
     cur = connection.cursor()
-    cur.execute("INSERT INTO Artpieces(name, artist_name, image_name, email, message, image_filename, artpiece_type) VALUES (?, ?, ?, ?, ?, ?, ?)", (username, artist_name, title, email, message, my_filename, type))
+    cur.execute(
+        "INSERT INTO Artpieces(name, artist_name, image_name, email, message, image_filename, artpiece_type) VALUES (?, ?, ?, ?, ?, ?, ?)",
+        (username, artist_name, title, email, message, my_filename, type))
     connection.commit()
     connection.close()
     return render_template("uploading_success.html",
@@ -181,7 +211,6 @@ def process():
                            image_address="/static/uploaded-images/" + my_filename,
                            email=email,
                            message=message)
-
 
 
 @app.route('/social')
@@ -198,6 +227,7 @@ def servers():
 
     return render_template('social.html', server_records=listed_server_records)
 
+
 @app.route('/server_uploading')
 def server_uploading():
     return render_template('secret_server_upload_page_discord_1280.html')
@@ -210,7 +240,6 @@ def secret_upload():
     requested_link_to_server = request.values.get("link_to_server")
     requested_server_description = request.values.get("server_description")
 
-
     requested_icon_image_file = request.files['server_icon_filename']
     my_icon_filename = secure_filename(requested_icon_image_file.filename)
 
@@ -221,13 +250,14 @@ def secret_upload():
     cur = connection.cursor()
     cur.execute(
         "INSERT INTO Servers (server_name, server_charact, link_to_server, server_icon_filename, server_description) VALUES (?, ?, ?, ?, ?)",
-        (requested_server_name, requested_server_charact, requested_link_to_server, my_icon_filename, requested_server_description))
+        (requested_server_name, requested_server_charact, requested_link_to_server, my_icon_filename,
+         requested_server_description))
     connection.commit()
     connection.close()
 
     return render_template('uploading_icon_success.html')
 
+
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host="0.0.0.0", port=5000, debug=True)
-
